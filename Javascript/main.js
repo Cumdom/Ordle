46e1801aa6df0b07bcd4bd9ecefe50fc635a094c
James <99767472+Cumdom@users.noreply.github.com>
2022-04-18 21:03:26 +0200

    f

---
+++ Javascript/main.js
@@ -0,0 +1,17 @@
+function yggdrasil(){
+    bootGame()
+
+}
+document.cookie = "username=John Doe;";
+
+
+
+document.getElementById('tb').addEventListener('click',tFUNC)
+
+var xd = 0;
+function tFUNC(e){
+    document.cookie = 'ard = 200;'
+    xd++
+    console.log('dicks')
+    document.getElementById('tb').innerHTML = document.cookie;
+}
