766f947d4ef8d43a506b58752022b12174d143d1
James <bendik.james@gmail.com>
2022-04-18 20:56:16 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,16 +2,14 @@ function yggdrasil(){
     bootGame()
 
 }
-document.cookie = "username=John Doe;";
 
 
-
-document.getElementById('tb').addEventListener('click',tFUNC)
-
-var xd = 0;
-function tFUNC(e){
-    document.cookie = 'ard = 200;'
-    xd++
-    console.log('dicks')
-    document.getElementById('tb').innerHTML = document.cookie;
-}
+// document.cookie = "username=John Doe;";
+// document.getElementById('tb').addEventListener('click',tFUNC)
+// var xd = 0;
+// function tFUNC(e){
+//     document.cookie = 'ard = 200;'
+//     xd++
+//     console.log('dicks')
+//     document.getElementById('tb').innerHTML = document.cookie;
+// }
